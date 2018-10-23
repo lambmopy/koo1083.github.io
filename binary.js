db682864d79225c7ebb2a306fa56b2127a48c65a
koo1083 <43218156+koo1083@users.noreply.github.com>
2018-10-23 14:40:17 -0500

    Update binary.js

--- binary.js
+++ binary.js
@@ -1,4 +1,40 @@
+function twoToTen()
+{
+    num = Number(document.project3.num1.value);
+    base = 2; //base or 'num' to convert to base 10
+    tenNum = 0; //stores number to be output at the end
+    numLength = num.toString().length; //converts num to a STRING, then counts the number of characters in the string
+    //in this case, length of '1101' is 4
 
+    while(numLength > 0)
+    {
+        var denominator = Math.pow(10, (numLength-1)); //gets denominator of a fraction
+        //for 1001, denominators will be 1000, 100, 10, 1
+        var currentDigit = Math.floor(num/denominator); //captures a 1 or 0 to multiply by 2^numLength
+        // above means 1001/1000 = 1.... 101/100 = 1.... 01/10 = 0.... 1/1 = 1....
+        tenNum = tenNum + currentDigit*Math.pow(base, numLength-1);
+        //currentDigit*2^3 ... currentDigit*2^2.... currentDigit*2^2.... etc
+        num = num%denominator; //reduces num for the next iteration
+        // 1101 -> 101 - > 01 -> 1
+        numLength--; //decreases the numLength value by 1... otherwise will loop infinitely.
+    }
+    return tenNum;
+}
+
+function tenToTwo()
+{
+    num = Number(document.project3.num2.value); //number to convert
+    base = 2; //destination base
+    smallNum = ""; //store remainders here
+
+    while (num > 0) //continue looping while num isn't zero
+    {
+        smallNum = num%base + smallNum; //3%5 = 2... % returns remainder
+        //setup num for the next iteration of the loop.
+        num = Math.floor(num/base);
+    }
+    return smallNum;
+}
 function smallToTen(num, base) //convert a small-base number to base-10
 {
     num = Number(document.project4.num.value);
